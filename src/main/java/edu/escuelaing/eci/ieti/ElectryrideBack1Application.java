b2d237ea78366d3543cf8fd53dfd4ce0bffc0cc0
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-02-28 17:10:52 -0500

    :hammer: base e integracion sonar

---
+++ src/main/java/edu/escuelaing/eci/ieti/ElectryrideBack1Application.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.eci.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectryrideBack1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectryrideBack1Application.class, args);
+	}
+
+}
