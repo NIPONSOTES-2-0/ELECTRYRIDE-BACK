ae8f90822af9ef67c8764345757bd16509c59058
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-02-28 20:13:50 -0500

    :recycle: cambiando a graddle

--- src/main/java/edu/escuelaing/eci/ieti/ElectryrideBack2Application.java
+++ src/main/java/edu/escuelaing/eci/ieti/ElectryrideBack2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElectryrideBack1Application {
+public class ElectryrideBack2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElectryrideBack1Application.class, args);
+		SpringApplication.run(ElectryrideBack2Application.class, args);
 	}
 
 }
