19a3e95d54ef1c2a198a2bf3a1b616ee1bf6a378
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-03-02 00:25:01 -0500

    :sparkles: add test

--- src/test/java/edu/escuelaing/eci/ieti/ElectryrideBack2ApplicationTests.java
+++ src/test/java/edu/escuelaing/eci/ieti/ElectryrideBack2ApplicationTests.java
@@ -1,13 +1,24 @@
 package edu.escuelaing.eci.ieti;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import edu.escuelaing.eci.ieti.services.UserServices;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ElectryrideBack2ApplicationTests {
 
+	@Autowired
+	UserServices userS; 
+	
 	@Test
-	void contextLoads() {
+	void consultaTablaUsuarios() {
+		assertTrue(userS.getUsers()!=null);
 	}
-
 }
