892b6be53823068cdacbfaf4e4c3e1be22f8572a
Eduard Jimenez <44656416+EdKillah@users.noreply.github.com>
2021-03-06 21:49:47 -0500

    Parking crud, update parkings

---
+++ src/main/java/edu/escuelaing/eci/ieti/services/ParkingService.java
@@ -0,0 +1,15 @@
+package edu.escuelaing.eci.ieti.services;
+
+import java.util.List;
+
+import edu.escuelaing.eci.ieti.models.Parking;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ParkingService {
+    
+    List<Parking> getParkings();
+
+    void rentedBike(String id);
+
+}
